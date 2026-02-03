f023ca7356c28e199531615ebe3297f884cb762c
anhson1207 <anhsondoraemon@gmail.com>
2026-02-03 19:29:13 +0700

    assignment30

--- BaiTapBuoi30/main.ts
+++ BaiTapBuoi30/main.ts
@@ -1,8 +1,8 @@
 import { ClassRoom } from "./modules/classRoom.js";
 import { Student } from "./modules/student.js";
-const classA=new ClassRoom("Lớp A");
-const studentA=new Student("An");
-const studentB=new Student("Binh");
+const classA:ClassRoom=new ClassRoom("Lớp A");
+const studentA:Student=new Student("An");
+const studentB:Student=new Student("Binh");
 classA.addStudent(studentA);
 classA.addStudent(studentB);
 classA.postAnnouncement("ngày mai kiểm tra OOP nhé các em");
