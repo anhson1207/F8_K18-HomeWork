83f17623108653236239b50db28b105620b2ae6a
anhson1207 <anhsondoraemon@gmail.com>
2026-04-10 09:21:38 +0700

    assignment40

---
+++ BaiTapBuoi40/2026-04-07/src/main.jsx
@@ -0,0 +1,10 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import Table from './Table.jsx'
+
+// board
+
+createRoot(document.getElementById('root')).render(
+  <Table />
+)
